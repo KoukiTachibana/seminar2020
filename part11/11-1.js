d374e1d67e38b648e24845af0298bec6403e4566
KOUKI_T <kt11158171v2@gmail.com>
2020-06-02 18:39:12 +0900

    11-1 end2

--- part11/11-1.js
+++ part11/11-1.js
@@ -1,7 +1,7 @@
 function buttonPressed() {
     var html = "";
     for (var i = 1; i <= 10; i++) {
-        var x = Math.floor(Math.random() * 7)
+        var x = Math.floor(Math.random() * 6 + 1)
         html = html + '<li>' + x + '</li>';
     }
     document.getElementById('ran').innerHTML = html;
