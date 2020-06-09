1512944f03e59e756935c4bf7f6f59349ce6cd4e
KOUKI_T <kt11158171v2@gmail.com>
2020-06-09 17:27:09 +0900

    11-5 con1

---
+++ part11/11-5.js
@@ -0,0 +1,10 @@
+function buttonPressed() {
+
+    var x1 = document.getElementById('input-base').Value;
+    x = Number(x1);
+    var y2 = document.getElementById('input-interst').Value;
+    y = Number(y2);
+
+    var html = "";
+    html = html + '<table border="1">';
+}
