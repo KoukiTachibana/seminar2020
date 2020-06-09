518d658dc693349df447da25ac518e80fc44c950
KOUKI_T <kt11158171v2@gmail.com>
2020-06-09 19:25:16 +0900

    11-5 end

--- part11/11-5.js
+++ part11/11-5.js
@@ -1,10 +1,37 @@
 function buttonPressed() {
 
-    var x1 = document.getElementById('input-base').Value;
-    x = Number(x1);
-    var y2 = document.getElementById('input-interst').Value;
-    y = Number(y2);
+    var x = document.getElementById('input-base').value;
+    x = Number(x);
+    var y = document.getElementById('input-interest').value;
+    y = Number(y);
+    y = y / 100;
+
+    var sum = x + (x * y + 1);
 
     var html = "";
     html = html + '<table border="1">';
+
+
+    for (var a = 1; a <= 10; a++) {
+
+        if (a == 1) {
+            html = html + '<tr>';
+            html = html + '<td>' + a + '</td>';
+            html = html + '<td>' + sum + '</td>';
+            html = html + '</tr>';
+        }
+        else {
+            html = html + '<tr>';
+            html = html + '<td>' + a + '</td>';
+            html = html + '<td>' + pre + '</td>';
+            html = html + '</tr>';
+        }
+        var pre = sum + (sum * y + 1)
+        sum = pre;
+    }
+
+
+    html = html + '</table>';
+    document.getElementById('tab').innerHTML = html;
+
 }
